0cd1d31760671c7e956c6f20864654abaabb11cb
leedonghyun1 <ehdgus4119@naver.com>
2023-10-17 22:31:25 +0900

    Onboarding Screens
    Onboarding Screens

---
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainNaviagtionScreen extends StatelessWidget {
+  const MainNaviagtionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
